5e53d196228c016045be8d95c630e0bfc73d087f
The-ultra <ahmedhamouda55@gmail.com>
2021-11-27 20:02:43 +0200

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,127 @@
+import React from "react";
+import ViewFlight from "./components/ViewFlight";
+import CreateFlight from "./components/CreateFlight";
+import UpdateFlight from "./components/UpdateFlight";
+import Delete from "./components/Delete";
+import SearchFlight from "./components/SearchFlight";
+
+import axios from "axios";
+// import UserCard from "../components/UserCard";
+// import Grid from '@mui/material/Grid';
+import { useState, useEffect } from "react";
+const { route, port } = require("./base");
+export default function App() {
+  const [showCreateForm, setShowCreateForm] = useState(false);
+  const [showList, setShowList] = useState(false);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [showSearchForm, setShowSearchForm] = useState(false);
+  const [showDeleteForm, setShowDeleteForm] = useState(false);
+  //const [list1,setList1] = useState([]);
+
+  const [list, setList] = useState([]);
+  //useEffect(()=>{},[]);
+  useEffect(() => {
+    axios
+      .get(`${route}:${port}/api/v1/flights`)
+      .then((res) => {
+        // console.log("frontend");
+        // console.log(res.data.data.flights);
+        setList(res.data.data.flights);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  /*useEffect(()=>{
+  axios.patch(`${route}:${port}/api/v1/flights`)
+    .then(res => {
+      console.log("frontend");
+      console.log(res.data.data.flights);
+      setList1(res.data.data.flights);
+    }).catch(err => console.log(err))
+
+
+},[]);
+*/
+
+  function cancelationEmail(){
+
+
+
+
+  }
+  function handleUpdate() {
+    //patch
+    setShowUpdateForm(true);
+  }
+  function handleSearch() {
+    setShowSearchForm(true);
+  }
+
+  return (
+    <>
+      <ViewFlight
+        setShowCreateForm={setShowCreateForm}
+        setShowList={setShowList}
+        setShowUpdateForm={setShowUpdateForm}
+        setShowDeleteForm={setShowDeleteForm}
+        setShowSearchForm={setShowSearchForm}
+      />
+      {showCreateForm ? <CreateFlight /> : <></>}
+      {<button onClick={handleSearch}> Search Flight </button>}
+
+      {showSearchForm ? <SearchFlight /> : <></>}
+
+      {showList ? (
+        list.map((a) => {
+          return (
+            <div>
+              <label for="FlightNumber"> FlightNumber:</label>
+              <label>{a.FlightNumber}</label>
+
+              <label for="DepartureTime"> DepartureTime:</label>
+              <label>{a.DepartureTime}</label>
+
+              <label for="ArrivalTime"> ArrivalTime:</label>
+              <label>{a.ArrivalTime}</label>
+
+              <label for="DateOfFlight"> DateOfFlight:</label>
+              <label>{a.DateOfFlight}</label>
+
+              <label for="NumberOfEconomySeats"> NumberOfEconomySeats:</label>
+              <label>{a.NumberOfEconomySeats}</label>
+
+              <label for="NumberOfBusinessSeats"> NumberOfBusinessSeats:</label>
+              <label>{a.NumberOfBusinessSeats}</label>
+
+              <label for="NumberOfFirstSeats"> NumberOfFirstSeats:</label>
+              <label>{a.NumberOfFirstSeats}</label>
+
+              <label for="Airport"> Airport:</label>
+              <label>{a.Airport}</label>
+
+              <label for="AirportTerminals"> AirportTerminals:</label>
+              <label>{a.AirportTerminals}</label>
+
+              <label for="From"> From:</label>
+              <label>{a.From}</label>
+
+              <label for="To"> To:</label>
+              <label>{a.To}</label>
+              <p>
+                <br></br>
+              </p>
+
+              <button onClick={handleUpdate}>Update </button>
+              {showUpdateForm ? <UpdateFlight id={a._id} /> : <></>}
+              {setShowDeleteForm ? <Delete id={a._id} /> : <></>}
+            </div>
+          );
+        })
+      ) : (
+        <></>
+      )}
+    </>
+  );
+
+  //export default App;
+}
---
+++ frontend/src/components/CreateFlight.js
@@ -0,0 +1,159 @@
+import { useState } from "react";
+import axios from "axios";
+const CreateFlight = () => {
+  const [FlightNumber, setFlightNumber] = useState();
+  const [DepartureTime, setDepartureTime] = useState();
+  const [ArrivalTime, setArrivalTime] = useState();
+  const [DateOfFlight, setDateOfFlight] = useState();
+  const [NumberOfEconomySeats, setNumberOfEconomySeats] = useState();
+  const [NumberOfBusinessSeats, setNumberOfBusinessSeats] = useState();
+  const [NumberOfFirstSeats, setNumberOfFirstSeats] = useState();
+  const [Airport, setAirport] = useState();
+  const [AirportTerminals, setAirportTerminals] = useState();
+  const [From, setFrom] = useState();
+  const [To, setTo] = useState();
+  //const [flightDate, setFlightDate] = useState()
+
+  console.log(FlightNumber);
+  const sub = function (e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/v1/flights", {
+        FlightNumber: FlightNumber,
+        DepartureTime: DepartureTime,
+        ArrivalTime: ArrivalTime,
+        DateOfFlight: DateOfFlight,
+        NumberOfEconomySeats: NumberOfEconomySeats,
+        NumberOfBusinessSeats: NumberOfBusinessSeats,
+        NumberOfFirstSeats: NumberOfFirstSeats,
+        Airport: Airport,
+        AirportTerminals: AirportTerminals,
+        From: From,
+        To: To,
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <form>
+      <label for="FlightNumber">FlightNumber:</label>
+      <input
+        type="text"
+        id="FlightNumber"
+        name="FlightNumber"
+        onChange={(event) => setFlightNumber(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="DepartureTime">Departure:</label>
+      <input
+        type="text"
+        id="DepartureTime"
+        name="DepartureTime"
+        onChange={(event) => setDepartureTime(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="ArrivalTime">Arrival:</label>
+      <input
+        type="text"
+        id="ArrivalTime"
+        name="ArrivalTime"
+        onChange={(event) => setArrivalTime(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="DateOfFlight">Date:</label>
+      <input
+        type="text"
+        id="DateOfFlight"
+        name="DateOfFlight"
+        onChange={(event) => setDateOfFlight(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="NumberOfEconomySeats">NumberOfEconomySeats:</label>
+      <input
+        type="text"
+        id="NumberOfEconomySeats"
+        name="NumberOfEconomySeats"
+        onChange={(event) => setNumberOfEconomySeats(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="NumberOfBusinessSeats">NumberOfBusinessClassSeats:</label>
+      <input
+        type="text"
+        id="NumberOfBusinessSeats"
+        name="NumberOfBusinessSeats"
+        onChange={(event) => setNumberOfBusinessSeats(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="NumberOfFirstSeats">NumberOfFirstSeats:</label>
+      <input
+        type="text"
+        id="NumberOfFirstSeats"
+        name="NumberOfFirstSeats"
+        onChange={(event) => setNumberOfFirstSeats(event.target.value)}
+      />
+
+      <p>
+        <br></br>
+      </p>
+      <label for="Airport">Airport:</label>
+      <input
+        type="text"
+        id="Airport"
+        name="Airport"
+        onChange={(event) => setAirport(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="AirportTerminals">AirportTerminals:</label>
+      <input
+        type="text"
+        id="AirportTerminals"
+        name="AirportTerminals"
+        onChange={(event) => setAirportTerminals(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="From">From:</label>
+      <input
+        type="text"
+        id="From"
+        name="From"
+        onChange={(event) => setFrom(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+      <label for="To">To:</label>
+      <input
+        type="text"
+        id="To"
+        name="To"
+        onChange={(event) => setTo(event.target.value)}
+      />
+      <p>
+        <br></br>
+      </p>
+
+      <button type="Create Flight" value="Create Flight" onClick={sub}>
+        create flight{" "}
+      </button>
+    </form>
+  );
+};
+
+export default CreateFlight;
