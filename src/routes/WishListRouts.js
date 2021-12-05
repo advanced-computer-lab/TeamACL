38abd5133890d5e70cdc7b3630e4d89b0f0d71bd
Faridashraf99 <86149945+Faridashraf99@users.noreply.github.com>
2021-12-05 11:42:40 +0200

    routs

---
+++ src/routes/WishListRouts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const WishListController = require("./../controllers/wishListController");
+
+const router = express.Router();
+router
+  .route(
+    "/:FlightNumber/:DepartureTime/:ArrivalTime/:DepartureDate/:ArrivalDate/:DurationInHrs/:DateOfFlight/:NumberOfEconomySeats/:NumberOfBusinessSeats/:NumberOfFirstClassSeats/:Airport/:DepartureTerminals/:ArrivalTerminals/:From/:To/:TotalTicketPrice/:BaggageAllowance/:TypeOfFlight/:Cabin"
+  )
+  .post(WishListController.addToWishList);
+
+router.route("/").get(WishListController.getWishList);
+
+module.exports = router;
---
+++ src/routes/reservationsRouts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const reserveController = require("./../controllers/ReservationsController");
+const flightController = require("./../controllers/flightController");
+
+const router = express.Router();
+
+router.route("/flightDetails/:id").get(flightController.getSelectedFlight); //gets the flight details using the getSelected flight method in the flight controller!!!
+
+router
+  .route("/:UserId/:FlightId/:FlightNumber")
+  .get(reserveController.getReservation);
+
+// UserEmail/:UserId/:FlightId
+router
+  .route("/:UserEmail/:UserId/:FlightId/:FlightNumber")
+  .post(reserveController.createReservation);
+
+router.route("/:id").delete(reserveController.canceleReservation);
+
+module.exports = router;
