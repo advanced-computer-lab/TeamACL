530ca014623bcc078b906a1e4c2066f8e1e8b409
Faridashraf99 <86149945+Faridashraf99@users.noreply.github.com>
2021-12-05 12:36:41 +0200

    error

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const flightRouts = require("./src/routes/flightRouts");
 const userRouter = require("./src/routes/usersRouts");
-// const reserveRouter = require("./src/routes/reservationsRouts");
-// const WishListRouter = require("./src/routes/WishListRouts");
+const reserveRouter = require("./src/routes/reservationsRouts");  //
+const WishListRouter = require("./src/routes/WishListRouts");  //
 const cors = require("cors");
-// const User = require("./src/models/User");
-// const Flight = require("./src/models/Flight");
+const User = require("./src/models/User");   //
+const Flight = require("./src/models/Flight");  //
 
 const app = express();
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 const bp= require('body-parser')
 app.use(bp.json())
 app.use(bp.urlencoded())
@@ -21,6 +21,6 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/flights", flightRouts);
 app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/reserve", reserveRouter);
-// app.use("/api/v1/WishList", WishListRouter);
+app.use("/api/v1/reserve", reserveRouter); //
+app.use("/api/v1/WishList", WishListRouter); //
 module.exports = app;
