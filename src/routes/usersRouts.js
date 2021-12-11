2b0fbab8d36bd81f5b6d5f988b91627fb916633f
omar-vtc <komar4727@gmail.com>
2021-12-11 15:16:41 +0200

    kjdsfhks

--- src/routes/usersRouts.js
+++ src/routes/usersRouts.js
@@ -3,11 +3,11 @@ const userController = require("./../controllers/userController");
 
 const router = express.Router();
 
- router.route("/:id").put(userController.updateUser); //di 
+router.route("/:id").put(userController.updateUser); //di
+
+router.route("/ViewProfile").get(userController.viewUser);
+router.route("/EditProfile").put(userController.editUser);
 
 router.route("/").post(userController.createUser);
-
-// router.route("/ViewProfile").get(userController.viewUser)
-// router.route("/EditProfile").put(userController.editUser)
 
 module.exports = router;
