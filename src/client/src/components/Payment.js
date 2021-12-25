618b50269c1dfab1d26e718ed6f2bba7df3086af
The-ultra <ahmedhamouda55@gmail.com>
2021-12-26 00:02:00 +0200

    Update Payment.js

--- src/client/src/components/Payment.js
+++ src/client/src/components/Payment.js
@@ -4,47 +4,60 @@ import React from "react";
 import { useState } from "react";
 import "./Payment.css";
 import { useNavigate, useParams, Link } from "react-router-dom";
+import StripeCheckout from "react-stripe-checkout";
+require('dotenv').config();
 
 function Payment() {
     let navigate = useNavigate();
     const [CardholderName, setCardholderName] = useState("");
     const [CardNumber, setCardNumber] = useState("");
     const [CVV, setCVV] = useState("");
-    const [Password, setPassword] = useState("");
-    const [PhoneNumber, setPhoneNumber] = useState("");
-    const [CountryCode, setCountryCode] = useState("");
-    const [HomeAddress, setHomeAddress] = useState("");
-    const [PassportNumber, setPassportNumber] = useState("");
-    const [Age, setAge] = useState("");
-    const [BornIn, setBornIn] = useState("");
-    const [LivesIn, setLivesIn] = useState("");
-    const [MartialStatus, setMartialStatus] = useState("");
-    const [Job, setJob] = useState("");
+    const[product,setProduct] = useState({
+      name: "Flight",
+      Price: 10000,
+      productBy: "Gair",
+    });
+    const makePayment = token  =>{
+      const body ={
+          token,
+          product
+
+      }
+      const headers={
+        "content-Type": "application/json"
+
+      }
+      return axios
+      .post("http://localhost:3000/api/v1/reservation/payment", {
+        body:JSON.stringify(body)
+ 
+      })
+      .then((response) => {
+        console.log("RESPONSE",response)
+        const{status} = response
+        console.log("STATUS",status)
+      }
+      ).catch(error => console.log(error))
     
-    
-    
-    function pay() {
-      console.log(Age);
-      axios
-        .post("http://localhost:3000/api/v1/reservation/Payment", {
-          HomeAddress,
-          CountryCode,
-          PassportNumber,
-          PhoneNumber,
-          Age,
-          Password,
-          BornIn,
-          LivesIn,
-          MartialStatus,
-          Job,
-        })
-        .then((res) => {
-          window.location.href = "/login";
-        });
+
     }
+    
+  
     return (
 <div>
-      <h2>{}</h2>
+      <h2>
+        <StripeCheckout stripeKey={process.env.REACT_APP_KEY}
+          token= ""
+          name="Flight"
+          ammount= {product.Price*100}
+>
+<button className= "btn-large pink">Buy flight</button>
+
+</StripeCheckout>
+        
+        
+        
+        </h2>
       <div className="login">
         <div className="child">
           <form>
@@ -102,6 +115,10 @@ function Payment() {
     </div>
   );
 
-   
+  function Pay() {
+    
+  }
 }
+
+
     export default Payment;
